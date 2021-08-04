fe49d1247bac0e8b49337e034ef8aaf6c21489d0
carlkiptoo <carlos.kirui@student.moringaschool.com>
2021-08-04 13:13:18 +0300

    add all changes

--- pizza.js
+++ pizza.js
@@ -35,6 +35,7 @@ const pizza= ['hawaian', 'cheese', 'meat']
 const topping= ['mushroom', 'onions', 'sausage', 'bacon', 'extra cheese', 'black olives', 'green peppers']
 const size= ['small', 'medium', 'large']
 
+
 var order = {
     fName: 'name',
     address: 'address',
@@ -48,6 +49,7 @@ order.size; {
     large= 1200
 }
 order.topping; {
+    pepperoni = 200
     mushroom= 200
     onions= 200
     sausage= 200
@@ -56,6 +58,9 @@ order.topping; {
    blackOlives= 200
    greenPeppers= 200
 }
+order.crust; {
+    stuffedCrust = 150
+}
 function order(pizza, topping, size) {
     this.pizza = pizza;
     this.topping = topping;
@@ -63,12 +68,15 @@ function order(pizza, topping, size) {
     
 }
 function myOrder() {
+    var pizzaPrice = [500, 800, 1200]
+    var toppingPrize = [200]
+    var crustPrize = [150]
     var name = document.getElementById('name').value;
     var address = document.getElementById('address').value;
     var pizza = document.getElementById('pizzaOne').value;
     var size = document.getElementById('size').value;
     var topping = document.getElementById('topping').value;
-    if(pizza == 'large' && topping == ''){
-        alert('Your order is large ' )
+    if(pizza == 'large' && topping == 'pepperoni' && crust == 'stuffed crust'){
+       document.getElementById('output').innerHTML = (pizzaPrice[2]+ toppingPrize[0]+ crustPrize[0])
     }
 }
